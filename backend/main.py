ab9ac0dda996c2dfc4fad50b58849532e0aff3b5
Aarna-09 <166246832+Aarna-09@users.noreply.github.com>
2026-04-27 22:43:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,44 +8,13 @@
 
 # load_dotenv()
 
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get("/")
-# def root():
-#     return {"status": "PawCare API running!"}
-
-# @app.post("/analyze")
-# async def analyze(file: UploadFile = File(...)):
-#     contents = await file.read()
-#     mime_type = file.content_type
-    
-#     if mime_type.startswith("video/"):
-#         result = analyze_pet_video(contents)
-#     else:
-#         result = analyze_pet(contents, mime_type)
-    
-#     return result
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-
-
-
 from fastapi import FastAPI, File, UploadFile, Form
 from typing import List, Optional
 import json
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from ai_engine import analyze_pet, analyze_pet_video,analyze_pet_text
+from backend.ai_engine import analyze_pet, analyze_pet_video,analyze_pet_text
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 
